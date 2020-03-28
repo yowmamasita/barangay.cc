746e5a1ab0a3af4c92ea5cc2b99f9a565abf17f2
Ben Sarmiento <ben.sarmiento@foodpanda.com>
2020-03-28 22:21:50 +0800

    COnfigure gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-W8DWCK3",
+      },
+    },
   ],
 }
